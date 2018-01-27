2f7cb1448f06acf4148e9a32ed1b02753ab4a3c8
absinha <abhishek01.sinha@yahoo.com>
2018-01-27 22:01:49 +0530

    adding test003 in git repo

---
+++ src/test/java/com/SelVer381/Test003.java
@@ -0,0 +1,12 @@
+package com.SelVer381;
+
+public class Test003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is 3rd test in selenium");
+
+	}
+
+}
